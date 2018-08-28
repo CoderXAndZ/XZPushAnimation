e7aa4c1dd9c18fa906254c4c1250ea844099bcf5
ios_domain <1935722630@qq.com>
2018-08-28 16:59:36 +0800

    给evalution.view 添加 autoresizingMask，修改 分享页面的右侧为 safety area，手势修改为仅添加一次

--- XZPushAnimation/XZDetailViewController.swift
+++ XZPushAnimation/XZDetailViewController.swift
@@ -18,19 +18,24 @@ class XZDetailViewController: UIViewController {
     
     var startPoint: CGPoint = CGPoint.zero
     
-    /// 子控制器视图初始frame
-    lazy var originRect: CGRect? = {
-        let height = UIScreen.main.bounds.height -  bottomToolBar.frame.origin.y
+//    /// 子控制器视图初始frame
+//    lazy var originRect: CGRect? = {
+//        let height = UIScreen.main.bounds.height -  bottomToolBar.frame.origin.y
+//
+//        let frameValue = CGRect(x: UIScreen.main.bounds.origin.x, y: UIScreen.main.bounds.origin.y, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - height)
+//        print("frameValue：",frameValue,"\n")
         
-        let frameValue = CGRect(x: UIScreen.main.bounds.origin.x, y: UIScreen.main.bounds.origin.y, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - height)
-        print("frameValue：",frameValue,"\n")
-        return frameValue
-    }()
+//        return frameValue
+//        return tableView.frame
+//    }()
     
     private let storyBoard = UIStoryboard(name: "Main", bundle: nil)
     /// 宝贝评价控制器
     private lazy var evalution:XZProductEvaluationController? = {
         let evaluationController = storyBoard.instantiateViewController(withIdentifier: "XZProductEvaluationController") as? XZProductEvaluationController
+        // 添加手势
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panAction(_:)))
+        evaluationController?.view.addGestureRecognizer(panGesture)
         
         return evaluationController
     }()
@@ -38,22 +43,23 @@ class XZDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "返回_黑色"), style: .plain, target: self, action: #selector(backAction))
-        //
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DetailViewControllerCell")
+        setupDetailViewController()
     }
     
     /// 左上角返回
     @objc func backAction() {
-        
-        guard var originRect = self.originRect,
-            let evalution = self.evalution
+//        var originRect = self.originRect,
+        guard let evalution = self.evalution
         else { return }
         
         if hasRemoved == false {
             UIView.animate(withDuration: 0.25) {
-                originRect.origin.x = evalution.view.bounds.width
-                evalution.view.frame = originRect
+//                originRect.origin.x = evalution.view.bounds.width
+//                evalution.view.frame = originRect
+                
+//                var frameValue = self.tableView.frame
+//                frameValue.origin.x = view.frame.width
+                evalution.view.frame.origin.x = self.view.frame.width
                 
                 self.hasRemoved = true
             }
@@ -66,8 +72,8 @@ class XZDetailViewController: UIViewController {
     /// 手势
     @objc func panAction(_ sender: UIPanGestureRecognizer) {
         
-        guard let evalution = self.evalution,
-            var originRect = originRect
+        guard let evalution = self.evalution // ,
+//            var originRect = originRect
             else {
                 return
         }
@@ -81,10 +87,14 @@ class XZDetailViewController: UIViewController {
             let dx = currentPoint.x - startPoint.x
             
             if dx > 0 {
-                evalution.view.frame = CGRect(x: dx, y: 0, width: originRect.width, height: originRect.height)
+//                evalution.view.frame = CGRect(x: dx, y: 0, width: originRect.width, height: originRect.height)
+//                evalution.view.frame = CGRect(x: dx, y: tableView.frame.origin.y, width: tableView.frame.width, height: tableView.frame.height)
+                evalution.view.frame.origin.x = dx
             } else {
-                originRect.origin.x = 0
-                evalution.view.frame = originRect
+//                originRect.origin.x = 0
+//                evalution.view.frame = originRect
+//                evalution.view.frame = tableView.frame
+                evalution.view.frame.origin.x = 0
                 return
             }
         case .ended:
@@ -93,21 +103,27 @@ class XZDetailViewController: UIViewController {
             if dx > 0 {
                 UIView.animate(withDuration: 0.25, animations: {
                     if (dx > 45) { // 超过45就移除子视图
-                        originRect.origin.x = evalution.view.bounds.width
-                        
+//                        originRect.origin.x = evalution.view.bounds.width
+//                        evalution.view.frame = CGRect(x: self.tableView.frame.width, y: self.tableView.frame.origin.y, width: self.tableView.frame.width, height: self.tableView.frame.height)
+                        evalution.view.frame.origin.x = self.view.bounds.width
                         self.hasRemoved = true
                     } else {
-                        originRect.origin.x = 0
-                        
+//                        originRect.origin.x = 0
+                        evalution.view.frame.origin.x = 0
                         self.hasRemoved = false
                     }
-                    evalution.view.frame = originRect
+//                    evalution.view.frame.origin.x = originRect.origin.x
+                    
+//                    evalution.view.frame = originRect
                 }, completion: { (_) in
                     
                 })
             }else {
-                originRect.origin.x = 0
-                evalution.view.frame = originRect
+//                originRect.origin.x = 0
+//                evalution.view.frame = originRect
+//                evalution.view.frame = self.tableView.frame
+                
+                evalution.view.frame.origin.x = 0
             }
         default:
             break
@@ -175,20 +191,38 @@ extension XZDetailViewController {
     
     // 将子视图添加到页面
     func setupEvalutionView() {
-        guard let evalution = evalution,
-            let originRect = originRect
+        guard let evalution = evalution // ,
+//            let originRect = originRect
             else { return }
         
-        evalution.view.frame = originRect
-        evalution.view.frame.origin.x = originRect.width
+//        evalution.view.frame = originRect
+//        evalution.view.frame.origin.x = originRect.width
+        
+//        var frameValue = self.tableView.frame
+//        frameValue.origin.x = self.tableView.frame.width
+        
+        let xValue = view.frame.width
+        let yValue:CGFloat = 0
+        let w = view.frame.width
+        let h = view.frame.height - bottomToolBar.frame.height
+        
+        evalution.view.frame = CGRect.init(x: xValue, y: yValue, width: w, height: h)
+//            CGRect(x: xValue, y: yValue, width: w, height: h)
+//        evalution.view.frame = frameValue
+        
+        evalution.view.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleWidth.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue) | UInt8(UIViewAutoresizing.flexibleTopMargin.rawValue) |  UInt8(UIViewAutoresizing.flexibleLeftMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleRightMargin.rawValue)))
+        
         view.addSubview(evalution.view)
         addChildViewController(evalution)
         
         hasRemoved = false
-        
-        // 添加手势
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panAction(_:)))
-        evalution.view.addGestureRecognizer(panGesture)
+    }
+    
+    /// 设置页面
+    func setupDetailViewController() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "返回_黑色"), style: .plain, target: self, action: #selector(backAction))
+        //
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DetailViewControllerCell")
     }
     
 }
